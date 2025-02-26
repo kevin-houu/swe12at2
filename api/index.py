45600828a17cff3777eb7135af48cfd4a65dea86
kevin-houu <kevinhou1008@gmail.com>
2025-02-26 14:23:21 +1100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -34,7 +34,7 @@ app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(days=30)
 app.config['JWT_COOKIE_SECURE'] = True
 app.config['JWT_COOKIE_CSRF_PROTECT'] = False
 app.config['JWT_COOKIE_SAMESITE'] = 'Strict'
-app.config['DATABASE'] = os.getenv('DATABASE_PATH', 'jyra.db')
+app.config['DATABASE'] = os.path.join(os.getcwd(), 'jyra.db')
 app.config['JWT_TOKEN_LOCATION'] = ['cookies']
 app.config['JWT_ACCESS_COOKIE_PATH'] = '/api/'
 app.config['JWT_REFRESH_COOKIE_PATH'] = '/api/refresh'
